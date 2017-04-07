10eb635445dfef417cdb8baaf923bb7afe4d4000
Charles Peden <ccbpeden@warpmail.net>
2017-04-07 09:13:09 -0700

    add legislator model

---
+++ src/app/legislator.model.ts
@@ -0,0 +1,8 @@
+export class Legislator {
+  constructor ( public name:string,
+                public position:string,
+                public party:string,
+                public constituency: string[],
+                public weakness: string[],
+                public debt: number,
+}
